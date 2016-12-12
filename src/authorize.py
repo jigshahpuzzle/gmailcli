86397965a8a4909bbd4530e342862bdfb97cabbe
jigar <jigshahpuzzle@gmail.com>
2016-12-11 18:06:09 -0800

    added some prompt stuff

--- src/authorize.py
+++ src/authorize.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import httplib2
 import os
 
-from apiclient import discovery
+from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
@@ -47,24 +47,4 @@ def get_credentials():
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
-
-def main():
-    """Shows basic usage of the Gmail API.
-
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
-
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
 
--- src/prompt.py
+++ src/prompt.py
@@ -1,2 +1,35 @@
+from authorize import get_credentials
+
+
+'''
+Script that generates a CLI to work with gmail
+'''
 def prompt(): 
-	print "Prompt has started"
+	print "Welcome to GMail on terminal"
+	print "Type 'mm' to view a list of commands"
+	while True: 
+		userinp = raw_input("-- > ")
+
+
+
+'''
+Matches passed in user input into a command
+'''
+def extractCommand(userinp):
+	commandList = ['', '', '', '', ''] 
+
+
+'''
+
+'''
+def mm(): 
+	pass 
+
+'''
+A hashmap that maps strings to function pointers
+'''
+fMap = {
+	'mm' :  mm,
+}
+
+
